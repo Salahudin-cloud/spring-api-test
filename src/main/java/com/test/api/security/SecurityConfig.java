f44a84bd11dd4f2b6ed3432db22a4502a5c749c2
Salahudin-cloud <ayubi54321@gmail.com>
2025-05-05 10:36:30 +0700

    fixed changes origin

--- src/main/java/com/test/api/security/SecurityConfig.java
+++ src/main/java/com/test/api/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -30,6 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
         security.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(url -> url
                         .requestMatchers(
                                 "/api/v1/auth/login",
